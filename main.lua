2d1476e010f7312371594f03f61b2583ad450d49
Kyle Lexmond <github@kyl191.net>
2013-01-29 14:18:59 -0500

    Set the background to white, and the text to black

--- main.lua
+++ main.lua
@@ -1,27 +1,40 @@
 local dw = director.displayWidth
 local dh = director.displayHeight
+
+local r0 = director:createRectangle( {
+    x=dw/2, y=dh/2, w=dw, h=dh,
+    color=color.white
+    } )
 local arial = director:createFont("Arial18.fnt")
 local question = director:createLabel( {
     x = 0, y = dh - 16,
-    font = arial
+    font = arial, 
+    color=color.black
     } )
 
 labels = {}
 -- Not too sure about the layout - it seems to calculate the height needed to show the text, then creates the box, not the other way around
+-- LABEL JUSTIFICATION!
 labels[1] = director:createLabel( {
     x=dw*2/4, y=(dh*3/3)-16,
     font = arial,
-    wrapWidth=dw*2/4
+    wrapWidth=dw*2/4,
+    hAlignment = "left", vAlignment = "middle",
+    color = color.black
     } )
 labels[2] = director:createLabel( {
     x=dw*2/4, y=(dh*2/3)-16,
     font = arial,
-    wrapWidth=dw*2/4
+    wrapWidth=dw*2/4,
+    hAlignment = "left", vAlignment = "middle",
+    color = color.black
     } )
 labels[3] = director:createLabel( {
     x=dw*2/4, y=(dh*0/3)-16,
     font = arial,
-    wrapWidth=dw*2/4
+    wrapWidth=dw*2/4,
+    hAlignment = "left", vAlignment = "middle",
+    color = color.black
     } )
     
 -- Load the saved JSON string back
