167d8bcc15695d7c12470204198e4e2384ae7f86
Kyle Lexmond <github@kyl191.net>
2013-01-30 18:23:43 -0500

    Do the same for the landscape case

--- questions.lua
+++ questions.lua
@@ -27,32 +27,21 @@ if dw > dh then
         x = 0, y = dh - 16,
         font = arial,
         } )
-    labels[1] = director:createLabel( {
-        x=dw*2/4, y=(dh*3/3)-16,
-        font = arial,
+    for i=0,2,1 do
+        labels[i+1] = director:createLabel( {
+            x=dw*2/4, y=(dh*(3-i)/3)-16,
+            font = arial,
         } )
-    labels[2] = director:createLabel( {
-        x=dw*2/4, y=(dh*2/3)-16,
-        font = arial,
+        backgrounds[i+1] = director:createRectangle( {
+            x=dw*3/4, y=(dh*(5-2*i)/6), w = dw*2/4, h = (dh*1/3),
         } )
-    labels[3] = director:createLabel( {
-        x=dw*2/4, y=(dh*1/3)-16,
-        font = arial,
-        } )
-    backgrounds[1] = director:createLabel( {
-        x=dw*3/4, y=(dh*5/6), w = dw*2/4, h = (dh*1/3),
-        } )
-    backgrounds[2] = director:createLabel( {
-        x=dw*3/4, y=(dh*3/6), w = dw*2/4, h = (dh*1/3),
-        } )
-    backgrounds[3] = director:createLabel( {
-        x=dw*3/4, y=(dh*1/6), w = dw*2/4, h = (dh*1/3),
-        } )
+    end
 else
     dbg.print("taller than wide")
     question = director:createLabel( {
         x = 0, y = dh - 16,
         font = arial,
+    } )
     for i=0,2,1 do
         labels[i+1] = director:createLabel( {
             x=dw*2/4, y=(dh*(3-i)/3)-16,
